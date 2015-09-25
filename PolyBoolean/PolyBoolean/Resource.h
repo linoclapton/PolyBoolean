e68f5df03e6d3b8e587e284663c569a43057727d
alexaway <nonearoundme@gmail.com>
2015-09-25 13:58:09 +0800

    the first edition

---
+++ PolyBoolean/PolyBoolean/Resource.h
@@ -0,0 +1,87 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 PolyBoolean.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDB_WRITESMALL                  110
+#define IDB_WRITELARGE                  111
+#define IDB_MAIN                        112
+#define IDB_BUTTONS                     113
+#define IDB_FILELARGE                   114
+#define IDB_FILESMALL                   115
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_PolyBooleanTYPE             130
+#define IDR_PASTE_MENU                  151
+#define IDR_WINDOWS_MENU                152
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDS_EDIT_MENU                   306
+#define IDR_RIBBON                      307
+#define ID_WRITE_PASTEASHYPERLINK       32770
+#define ID_COMBO2                       32771
+#define ID_COMBO3                       32772
+#define ID_COMBO_AorB                   32773
+#define ID_EDGE_NUMBER                  32774
+#define ID_NEW_RIGHT_OUTLOOP            32775
+#define ID_NEW_RIGHT_INLOOP             32776
+#define ID_CHECK2                       32777
+#define ID_SELECT_POINT                 32778
+#define ID_SELECT_LOOP                  32779
+#define ID_SELECT_REGION                32780
+#define ID_SELECT_POLYGON               32781
+#define ID_SELECT_TRIANGLE              32782
+#define ID_SELECT_ONLY                  32783
+#define ID_BUTTON4                      32784
+#define ID_CHECK3                       32785
+#define ID_ADD_OUTLOOP                  32786
+#define ID_ADD_INLOOP                   32787
+#define ID_ADD_POINT                    32788
+#define ID_DELETE                       32789
+#define ID_MOVE_SAME                    32790
+#define ID_CHECK                        32791
+#define ID_BUTTON2                      32792
+#define ID_BUTTON3                      32793
+#define ID_POLYGON_B_A                  32794
+#define ID_POLYGON_TRIANGULATION        32795
+#define ID_POLYGON_UNION                32796
+#define ID_POLYGON_INTERSECTION         32797
+#define ID_POLYGON_A_B                  32798
+#define ID_VIEW_A                       32800
+#define ID_VIEW_B                       32801
+#define ID_VIEW_RESULT                  32802
+#define ID_VIEW_T_FACE                  32803
+#define ID_VIEW_T_EDGE                  32804
+#define ID_VIEW_STANDARD                32805
+#define ID_VIEW_POINT_ID                32806
+#define ID_VIEW_T_FACE_EDGE             32807
+#define ID_VIEW_FIT                     32808
+#define ID_VIEW_STAT                    32809
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32810
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
