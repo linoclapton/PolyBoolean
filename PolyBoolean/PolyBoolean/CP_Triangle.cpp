2bc860a179af175ae84c50f9951d30f1d2ecdbc9
alexaway <nonearoundme@gmail.com>
2015-09-25 14:16:50 +0800

    the first edition

---
+++ PolyBoolean/PolyBoolean/CP_Triangle.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+#include "CP_Triangle.h"
+
+void CP_TriagleMesh::mb_buildTriagleMesh(CP_Polygon& pn)
+{
+	m_polygon = &pn;
+	int n = pn.m_pointArray.size();
+	if (n<3)
+		return;
+	m_triagleIDArray.resize(1);
+	m_triagleIDArray[0].m_vertices[0] = 0;
+	m_triagleIDArray[0].m_vertices[0] = 1;
+	m_triagleIDArray[0].m_vertices[0] = 2;
+} // 类CP_TriagleMesh的成员函数mb_buildTriagleMesh结束
+
